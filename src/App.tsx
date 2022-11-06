79271ce5ff17f3e97c23186ab022a0d80c7dbcc7
sakun9526 <sakunrathnayaka@yahoo.com>
2022-11-06 12:45:42 +0530

    developing UI WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from "./App.module.css";
+import { FcSearch } from "react-icons/fc";
 
 function App() {
+  // const handleOnSubmit = () => {
+  //   alert("Hello");
+  // };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.App}>
+      <h1 className={styles.heading}>Woordle</h1>
+      <div className={styles.wrap}>
+        <div className={styles.search}>
+          <input
+            type="text"
+            name="name"
+            className={styles.synonymTextInput}
+            placeholder="Type a word to find synonyms?"
+          />
+          <button type="submit" className={styles.searchButton}>
+            <FcSearch />
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
