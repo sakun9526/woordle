0ea705d12d31d2d6542ebf31dc5d0aff9e98ea3b
sakun9526 <sakunrathnayaka@yahoo.com>
2022-11-08 09:05:18 +0530

    result container WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import styles from "./App.module.css";
 import { FcSearch } from "react-icons/fc";
+import { useState } from "react";
 
 function App() {
-  // const handleOnSubmit = () => {
-  //   alert("Hello");
-  // };
+  const [word, setWord] = useState<string>("");
+
+  const onSearch = () => {
+    fetch(`https://api.datamuse.com/words?rel_syn=${word}`)
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  };
 
   return (
     <div className={styles.App}>
-      <h1 className={styles.heading}>Woordle</h1>
+      <div className={styles.textContainer}>
+        <h1 className={styles.heading}>Woordle</h1>
+      </div>
       <div className={styles.wrap}>
         <div className={styles.search}>
           <input
@@ -16,10 +23,29 @@ function App() {
             name="name"
             className={styles.synonymTextInput}
             placeholder="Type a word to find synonyms?"
+            value={word}
+            onChange={(e) => setWord(e.target.value)}
           />
-          <button type="submit" className={styles.searchButton}>
+          <button
+            type="submit"
+            className={styles.searchButton}
+            onClick={onSearch}
+          >
             <FcSearch />
           </button>
+        </div>
+
+        <div className={styles.resultContainer}>
+          <div className={styles.results}>
+            <div className={styles.result}>Hello</div>
+            <div className={styles.result}>Hello</div>
+            <div className={styles.result}>Hello</div>
+            <div className={styles.result}>Hello</div>
+            <div className={styles.result}>Hello</div>
+            <div className={styles.result}>Hello</div>
+            <div className={styles.result}>Hello</div>
+            <div className={styles.result}>Hello</div>
+          </div>
         </div>
       </div>
     </div>
